da6585a0e93bfe2a9497f7f3926b0a283bbac72f
ACCOUNT-01\d970670 <Pradyumna.ShrivatsaKedilaya@team.telstra.com>
2021-09-14 15:02:31 +0530

    Final Commit

--- controllers/pauthorize.js
+++ controllers/pauthorize.js
@@ -1,4 +1,8 @@
 const fetch = require("node-fetch");
+const request = require('request');
+const {OAuth2Client} = require('google-auth-library')
+const client = new OAuth2Client("457453379813-1ei0s3u553o1elucdfbmhj6c8v6cknt7.apps.googleusercontent.com")
+
 
 module.exports = {
     verifyAuth: async function (req, res, next) {
@@ -10,36 +14,54 @@ module.exports = {
         }
         // Check if token is undefined
         else if (typeof token !== 'undefined') {
-            const response = await fetch(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${token}`);
-            const user = await response.json();
-            if ([user.error]=='invalid_token'){res.sendStatus(403);return;}
-            const userid = user.user_id;
-            dbo.collection('users').findOne({'Id': userid },(err,result)=>{if(result==null) {res.send('User Not Found');return}
-                if(result.Id == user_Id && result.Id){ 
+            //const response = await fetch(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${token}`);
+            //const user = await response.json();
+            request.get({
+                headers:{'content-type':'application/json'},
+                url:`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${token}`
+            },(err,response,body)=>{
+                if(err) throw err
+                //console.log(body);
+                body=JSON.parse(body)
+                if (body.user_id) { 
+                    //console.log(body);
                     next(); }
-                else if(err) {res.sendStatus(403);}
+                else if ([body.error]=='invalid_token'){res.sendStatus(403);}
+
+            
             })
         }
         else res.sendStatus(403)
     },
 
     verifyToken: async function (req, res, next) {
-    // Get auth header value
+        // Get auth header value
         const token = req.headers['x-access-token'];
-        // Check if bearer is undefined
-        if (typeof token !== 'undefined') {
-            const response = await fetch(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${token}`);
-            const user = await response.json();
-            if ([user.error]=='invalid_token'){res.sendStatus(403);return;}
-            const userid = user.user_id;
-            dbo.collection('users').findOne({'Id': userid },(err,result)=>{if(result==null) {res.send('User Not Found');return}
-                if(result.Id == userid){ 
+        const user_Id = String(req.params.user_id);
+        if (token == undefined || token == null || token.length <5) {
+            next();
+        }
+        // Check if token is undefined
+        else if (typeof token !== 'undefined') {
+            //const response = await fetch(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${token}`);
+            //const user = await response.json();
+            request.get({
+                headers:{'content-type':'application/json'},
+                url:`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${token}`
+            },(err,response,body)=>{
+                if(err) throw err
+                //console.log(body);
+                body=JSON.parse(body)
+                if (body.user_id) { 
+                    //console.log(body);
                     next(); }
+                else if ([body.error]=='invalid_token'){res.sendStatus(403);}
 
-                else if(err) {    
-                    res.sendStatus(403);}
+            
             })
-        }},
+        }
+        else res.sendStatus(403)
+    },
 
 
     validate_user: function (token,user_obj){
